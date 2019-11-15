c5987742a2814ed1bf7b553cf3e1ac4806597c82
litefeel <litefeel@gmail.com>
2019-11-15 18:00:02 +0800

    Update FindMissingOnCurrentScene.cs

--- Editor/Windows/FindMissingOnCurrentScene.cs
+++ Editor/Windows/FindMissingOnCurrentScene.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Experimental.SceneManagement;
 using UnityEditor.IMGUI.Controls;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityObject = UnityEngine.Object;
 
@@ -14,6 +16,26 @@ namespace litefeel.Finder.Editor
         {
             m_Items.Clear();
             m_ItemNames.Clear();
+
+            var list = new List<Transform>();
+            var stage = PrefabStageUtility.GetCurrentPrefabStage();
+            if(stage != null)
+            {
+                ForeachTransform(stage.prefabContentsRoot.transform, list);
+            }else
+            {
+                var count = EditorSceneManager.loadedSceneCount;
+                for (var i = 0; i < count; i++)
+                {
+                    var scene = EditorSceneManager.GetSceneAt(i);
+                    foreach(var go in scene.GetRootGameObjects())
+                    {
+                        ForeachTransform(go.transform, list);
+                    }
+                }
+                
+            }
+
             bool has = false;
             var activeObject = Selection.activeObject;
             if(activeObject != null && EditorUtility.IsPersistent(activeObject))
@@ -52,6 +74,21 @@ namespace litefeel.Finder.Editor
         protected override void OnItemDoubleClick(int index)
         {
             AssetDatabase.OpenAsset(m_Items[index]);
+        }
+
+        private void ForeachTransform(Transform trans, List<Transform> list)
+        {
+            foreach(var t in trans.gameObject.GetComponents<Component>())
+            {
+                if(t == null)
+                {
+                    list.Add(trans);
+                    break;
+                }
+            }
+            var count = trans.childCount;
+            for (var i = 0; i < count; i++)
+                ForeachTransform(trans.GetChild(i), list);
         }
 
     }
