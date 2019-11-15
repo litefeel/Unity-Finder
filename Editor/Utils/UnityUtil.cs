3e12759a9768c28ca7b46edcc1f725986ffea3a0
litefeel <litefeel@gmail.com>
2019-11-16 01:11:29 +0800

    Add UnityUtil

---
+++ Editor/Utils/UnityUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace litefeel.Finder.Editor
+{
+    static class UnityUtil
+    {
+        
+        public static string GetFullPath(Transform trans)
+        {
+            var list = ListPool<string>.Get();
+            while(trans != null)
+            {
+                list.Add(trans.name);
+                trans = trans.parent;
+            }
+            list.Reverse();
+            var fullpath = string.Join("/", list);
+            ListPool<string>.Release(list);
+            return fullpath;
+        }
+    }
+}
