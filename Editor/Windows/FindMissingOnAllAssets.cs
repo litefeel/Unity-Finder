d8866a1467c36c585436948e45f4931379bbf200
litefeel <litefeel@gmail.com>
2019-11-16 13:43:50 +0800

    Update FindMissingOnAllAssets.cs

--- Editor/Windows/FindMissingOnAllAssets.cs
+++ Editor/Windows/FindMissingOnAllAssets.cs
@@ -8,12 +8,24 @@ using UnityObject = UnityEngine.Object;
 
 namespace litefeel.Finder.Editor
 {
-    class FindMissingOnAllAssets : FindWindowBase<UnityObject>
+    class FindMissingOnAllAssets : FindWindowBase<UnityObject>, IHasCustomMenu
     {
+        public void AddItemsToMenu(GenericMenu menu)
+        {
+            menu.AddItem(new GUIContent("FindMissingOnSelection"), false, OpenWindow<FindMissingOnSelection>);
+            menu.AddItem(new GUIContent("FindMissingOnCurrentScene"), false, OpenWindow<FindMissingOnCurrentScene>);
+            menu.AddItem(new GUIContent("FindMissingOnAllAssets"), false, OpenWindow<FindMissingOnAllAssets>);
+        }
+
+        private void OpenWindow<T>() where T : EditorWindow
+        {
+            GetWindow<T>().Show();
+        }
+
         protected override bool InGameObject(GameObject prefab)
         {
             var comps = prefab.GetComponentsInChildren<Component>(true);
-            for(var i = 0; i < comps.Length; i++)
+            for (var i = 0; i < comps.Length; i++)
             {
                 if (comps[i] == null)
                     return true;
